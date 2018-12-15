0a3bebda0c590a4221100f30dfafb19611ff08a2
vishnuGopireddy <vishnu.var.reddy@gmail.com>
2018-12-15 19:24:34 +0530

    Binary Representaion of a number

---
+++ Bit Magic/Binary Representation.py
@@ -0,0 +1,14 @@
+# Binary Representation of Number with 14 bits
+def binary(num):
+    '''
+
+    :param num: num which has to be converted to binary
+    :return: Binary Representation
+    '''
+    return(format(num,'014b')) #convert to binary
+
+
+kases = int(input())
+for kase in range(kases):
+    num = int(input())
+    print(binary(num))
